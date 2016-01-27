fab9528ede7e284586e1e74634cbe4a739c45a48
Hannes Nyberg <hannes.nyberg@gmail.com>
2016-01-27 16:44:01 +0100

    Create forest, Burn trees

--- Frontend01/script.js
+++ Frontend01/script.js
@@ -4,13 +4,39 @@
 // 	https://github.com/hnyberg
 //
 $(document).ready(
-    function(){
-    	$('#addButton').click(
-    		function(){
-    		}
-    	);
+	function(){
+		$('#addButton').click(
+			function(event){
+				event.preventDefault();
+				if ($('input').val() !== ""){
+
+					// Create tree
+					var tree = "<li class='tree'>"
+					+ $('input').val()
+					+ "</li>";
+
+					// Plant tree
+					$('#forest').append(tree);
+
+					// Change tree color
+					var clr = 20 + Math.floor(Math.random()*40);
+					var treeColor = "#" + clr + (clr+20) + clr;
+					$('.tree').last().css("background-color", treeColor);
+
+					// Set click event handler on tree
+					$('.tree').last().on(
+						"click", function(){
+							$(this).css("background-color", "#DD9933");
+							$(this).addClass("burn");
+							$(this).fadeOut(
+								1000,function(){
+									$(this).remove();
+								}
+							);
+						}
+					);
+				}
+			}
+		);
     }
 );
-function buttonClicked(){
-	$('#itemList').append("<li class='listItem'>Hej</li>");
-}
